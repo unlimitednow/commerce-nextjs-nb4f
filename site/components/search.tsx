478cc31bb06d24687b5bfcc99ec046979e792bf9
Admin <support@unlimitpotential.com>
2023-04-17 14:15:42 -0400

    a

--- site/components/search.tsx
+++ site/components/search.tsx
@@ -225,22 +225,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                         }
                       )}
                     >
-                      <Link
-                        href={{
-                          pathname: getDesignerPath('', category),
-                          query,
-                        }}
-                        legacyBehavior
-                      >
-                        <a
-                          onClick={(e) => handleClick(e, 'brands')}
-                          className={
-                            'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                          }
-                        >
-                          All Designers
-                        </a>
-                      </Link>
+                     
                     </li>
                     {brands.map(({ path, name, id }: Brand) => (
                       <li
