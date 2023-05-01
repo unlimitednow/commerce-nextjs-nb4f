d28030dae1fe09cbe4d7a3fa6f2e3aebdebdf0b3
Admin <support@unlimitpotential.com>
2023-05-01 03:30:04 -0400

    i

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -27,7 +27,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Layout>
       </ManagedUIContext>
-      <div className="mx-auto container py-16 xl:px-20 lg:px-12 sm:px-6 px-4">
+      <div className="bg-white  mx-auto container py-16 xl:px-20 lg:px-12 sm:px-6 px-4">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 md:gap-8 gap-4">
           <div className="flex flex-col flex-shrink-0">
             <div>
