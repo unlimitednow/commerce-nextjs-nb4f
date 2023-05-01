b50cf17e40d8d866831ac93c6396e6a72ecaaea8
Admin <support@unlimitpotential.com>
2023-04-30 23:45:11 -0400

    s

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -7,6 +7,14 @@ import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
 import { builder, Builder, withChildren } from '@builder.io/react'
+import { Fragment, useState } from 'react'
+import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
 
 builder.init('ba26b1f01a7a45cdbbff41a67447be22')
 
