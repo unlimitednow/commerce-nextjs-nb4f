49137cc9cf41aee6691cbdd5764c19c8bd314116
Admin <support@unlimitpotential.com>
2023-05-01 17:59:49 -0400

    s

--- site/components/product/ProductSidebar/ProductSidebar.tsx
+++ site/components/product/ProductSidebar/ProductSidebar.tsx
@@ -10,7 +10,7 @@ import {
   SelectedOptions,
 } from '../helpers'
 import ErrorMessage from '@components/ui/ErrorMessage'
-import Badge from './Badge'
+import Badge from '../ProductSidebar/Badge'
 
 interface ProductSidebarProps {
   product: Product
