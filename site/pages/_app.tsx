b6e6200852bba58a913099b91e1c351b43dcd634
Admin <support@unlimitpotential.com>
2023-05-01 01:14:42 -0400

    s

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -344,7 +344,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           </Dialog>
         </Transition.Root>
 
-        <header className="relative bg-white">
+        <header className="relative bg-white z-40">
           <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
             Get free delivery on orders over $100
           </p>
