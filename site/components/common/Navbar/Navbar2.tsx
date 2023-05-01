11781ddbc4fb1e5afc3dabce67d5dfece690d7bd
Admin <support@unlimitpotential.com>
2023-05-01 03:03:08 -0400

    i

--- site/components/common/Navbar/Navbar2.tsx
+++ site/components/common/Navbar/Navbar2.tsx
@@ -509,12 +509,6 @@ const Navbar: FC<NavbarProps> = ({ links }) => {
                     </div>
                   )}
                   <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                    <a
-                      href="#"
-                      className="text-sm font-medium text-gray-700 hover:text-gray-800"
-                    >
-                      Wishlist
-                    </a>
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                     <a
                       href="#"
