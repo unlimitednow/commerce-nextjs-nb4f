68ebfc0f766cc4d6ae26eb0b6525a95fc7951834
Admin <support@unlimitpotential.com>
2023-05-01 02:41:08 -0400

    ss

--- site/components/product/ProductView/ProductView.tsx
+++ site/components/product/ProductView/ProductView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import cn from 'clsx'
 import Image from 'next/image'
 import s from './ProductView.module.css'
@@ -14,7 +15,49 @@ interface ProductViewProps {
   product: Product
   relatedProducts: Product[]
 }
-
+const products = [
+  {
+    id: 1,
+    name: 'Earthen Bottle',
+    href: '#',
+    price: '$48',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    imageAlt:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+  {
+    id: 2,
+    name: 'Nomad Tumbler',
+    href: '#',
+    price: '$35',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+    imageAlt:
+      'Olive drab green insulated bottle with flared screw lid and flat top.',
+  },
+  {
+    id: 3,
+    name: 'Focus Paper Refill',
+    href: '#',
+    price: '$89',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+    imageAlt:
+      'Person using a pen to cross a task off a productivity paper card.',
+  },
+  {
+    id: 4,
+    name: 'Machined Mechanical Pencil',
+    href: '#',
+    price: '$35',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageAlt:
+      'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  // More products...
+]
 const ProductView: FC<ProductViewProps> = ({ product, relatedProducts }) => {
   const { price } = usePrice({
     amount: product.price.value,
@@ -70,17 +113,35 @@ const ProductView: FC<ProductViewProps> = ({ product, relatedProducts }) => {
           <div className={s.relatedProductsGrid}>
             {relatedProducts.map((p) => (
               <div key={p.path} className="bg-accent-0 border border-accent-2">
-                <ProductCard
-                  noNameTag
-                  product={p}
-                  key={p.path}
-                  variant="simple"
-                  className="animated fadeIn"
-                  imgProps={{
-                    alt: p.name,
-                    className: 'w-full h-full object-cover',
-                  }}
-                />
+                <div className="bg-white">
+                  <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                    <h2 className="sr-only">Products</h2>
+
+                    <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                      {products.map((product) => (
+                        <a
+                          key={product.id}
+                          href={product.href}
+                          className="group"
+                        >
+                          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                            <img
+                              src={product.imageSrc}
+                              alt={product.imageAlt}
+                              className="h-full w-full object-cover object-center group-hover:opacity-75"
+                            />
+                          </div>
+                          <h3 className="mt-4 text-sm text-gray-700">
+                            {product.name}
+                          </h3>
+                          <p className="mt-1 text-lg font-medium text-gray-900">
+                            {product.price}
+                          </p>
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
