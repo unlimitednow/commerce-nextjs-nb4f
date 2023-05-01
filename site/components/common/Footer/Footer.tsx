aa3d59f71024dc4514b8b7ade55a5462cc536b7f
Admin <support@unlimitpotential.com>
2023-05-01 04:01:19 -0400

    a

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -51,26 +51,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
           <div className="bg-white">
             <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
               <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
-                  alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-                  className="rounded-lg bg-gray-100"
-                />
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
-                  alt="Top down view of walnut card tray with embedded magnets and card groove."
-                  className="rounded-lg bg-gray-100"
-                />
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
-                  alt="Side of walnut card tray with card groove and recessed card area."
-                  className="rounded-lg bg-gray-100"
-                />
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
-                  alt="Walnut card tray filled with cards and card angled in dedicated groove."
-                  className="rounded-lg bg-gray-100"
-                />
+                Hello
               </div>
             </div>
           </div>
