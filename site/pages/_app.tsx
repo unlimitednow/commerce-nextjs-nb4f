d270fa42b8ae285e40e5cf136cdb971b65b2d1b6
Admin <support@unlimitpotential.com>
2023-05-01 01:23:40 -0400

    f

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -348,6 +348,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
             Get free delivery on orders over $100
           </p>
+
+          <nav
+            aria-label="Top"
+            className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+          >
+            <div className="border-b border-gray-200"></div>
+          </nav>
         </header>
       </div>
       <ManagedUIContext>
