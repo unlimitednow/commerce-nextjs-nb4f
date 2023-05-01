a192fd9cc2386c1a2333a673ff91034c9c3f85f8
Admin <support@unlimitpotential.com>
2023-05-01 04:06:23 -0400

    s

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -345,7 +345,7 @@ const Navbar: FC<NavbarProps> = ({ links }) => {
         </Transition.Root>
 
         <header className="relative bg-white">
-          <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+          <p className="flex h-10 items-center justify-center bg-fuchsia-900 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
             Get free delivery on orders over $100
           </p>
 
