cd2a006ddaeee97c3f8c6eadb38d9e90878d9117
Admin <support@unlimitpotential.com>
2023-05-01 23:44:26 -0400

    s

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -36,7 +36,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             <button
               className="mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
               onClick={() => {
-                setTheme(theme === 'light' ? 'dark' : 'light')
+                setTheme(theme === 'light' ? 'light' : 'light')
               }}
             >
               Change Theme
