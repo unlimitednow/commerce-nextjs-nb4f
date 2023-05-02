b2b36f4dfbe527e81c936fde711347e69fb661ac
Admin <support@unlimitpotential.com>
2023-05-02 03:42:02 -0400

    h

--- site/components/product/ProductSidebar/ProductSidebar.tsx
+++ site/components/product/ProductSidebar/ProductSidebar.tsx
@@ -64,15 +64,17 @@ const ProductSidebar: FC<ProductSidebarProps> = ({ product, className }) => {
         selectedOptions={selectedOptions}
         setSelectedOptions={setSelectedOptions}
       />
-
       <div></div>
       <div className="mt-6">
         {`${price} ${product.price?.currencyCode}`}
         <br></br>
         {product.name}
-        <br></br>
-        Guaranteed safe & secure checkout<br></br>{' '}
-      </div>
+        <br></br>{' '}
+      </div>{' '}
+      <Text
+        className="pb-4 break-words w-full max-w-xl"
+        html={product.descriptionHtml || product.description}
+      />
       {error && <ErrorMessage error={error} className="my-5" />}
       {process.env.COMMERCE_CART_ENABLED && (
         <Button
@@ -87,12 +89,10 @@ const ProductSidebar: FC<ProductSidebarProps> = ({ product, className }) => {
             ? 'Not Available'
             : 'Add To Cart'}
         </Button>
-      )}
+      )}{' '}
       <br></br>
-      <Text
-        className="pb-4 break-words w-full max-w-xl"
-        html={product.descriptionHtml || product.description}
-      />
+      Guaranteed safe & secure checkout
+      <br></br>
       <div className="flex flex-row justify-between items-center">
         <Rating value={4} />
         <div className="text-accent-6 pr-1 font-medium text-sm">36 reviews</div>
