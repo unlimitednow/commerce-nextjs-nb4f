28f93d0bd6396442a197607a96fca0e97b2120b7
Admin <support@unlimitpotential.com>
2023-05-02 02:17:24 -0400

    h

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -346,7 +346,7 @@ const Navbar: FC<NavbarProps> = ({ links }) => {
 
         <header className="relative bg-white">
           <p className="flex h-10 items-center justify-center bg-fuchsia-900 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-           Now Available | Shop Dreika Romper Collection!
+            Now Available | Shop Dreika Romper Collection!
           </p>
 
           <nav
@@ -531,14 +531,14 @@ const Navbar: FC<NavbarProps> = ({ links }) => {
                         >
                           <path
                             d="M17 19V17C17 15.9391 16.5786 14.9217 15.8284 14.1716C15.0783 13.4214 14.0609 13 13 13H5C3.93913 13 2.92172 13.4214 2.17157 14.1716C1.42143 14.9217 1 15.9391 1 17V19"
-                            stroke="currentColor"
+                            stroke="black"
                             strokeWidth="1.5"
                             strokeLinecap="round"
                             strokeLinejoin="round"
                           />
                           <path
                             d="M9 9C11.2091 9 13 7.20914 13 5C13 2.79086 11.2091 1 9 1C6.79086 1 5 2.79086 5 5C5 7.20914 6.79086 9 9 9Z"
-                            stroke="currentColor"
+                            stroke="black"
                             strokeWidth="1.5"
                             strokeLinecap="round"
                             strokeLinejoin="round"
@@ -560,7 +560,7 @@ const Navbar: FC<NavbarProps> = ({ links }) => {
                         >
                           <path
                             d="M19.4578 2.59133C18.9691 2.08683 18.3889 1.68663 17.7503 1.41358C17.1117 1.14054 16.4272 1 15.7359 1C15.0446 1 14.3601 1.14054 13.7215 1.41358C13.0829 1.68663 12.5026 2.08683 12.0139 2.59133L10.9997 3.63785L9.98554 2.59133C8.99842 1.57276 7.6596 1.00053 6.26361 1.00053C4.86761 1.00053 3.52879 1.57276 2.54168 2.59133C1.55456 3.6099 1 4.99139 1 6.43187C1 7.87235 1.55456 9.25383 2.54168 10.2724L3.55588 11.3189L10.9997 19L18.4436 11.3189L19.4578 10.2724C19.9467 9.76814 20.3346 9.16942 20.5992 8.51045C20.8638 7.85148 21 7.14517 21 6.43187C21 5.71857 20.8638 5.01225 20.5992 4.35328C20.3346 3.69431 19.9467 3.09559 19.4578 2.59133V2.59133Z"
-                            stroke="currentColor"
+                            stroke="black"
                             strokeWidth="1.5"
                             strokeLinecap="round"
                             strokeLinejoin="round"
@@ -582,21 +582,21 @@ const Navbar: FC<NavbarProps> = ({ links }) => {
                         >
                           <path
                             d="M4 1L1 5V19C1 19.5304 1.21071 20.0391 1.58579 20.4142C1.96086 20.7893 2.46957 21 3 21H17C17.5304 21 18.0391 20.7893 18.4142 20.4142C18.7893 20.0391 19 19.5304 19 19V5L16 1H4Z"
-                            stroke="currentColor"
+                            stroke="black"
                             strokeWidth="1.5"
                             strokeLinecap="round"
                             strokeLinejoin="round"
                           />
                           <path
                             d="M1 5H19"
-                            stroke="currentColor"
+                            stroke="black"
                             strokeWidth="1.5"
                             strokeLinecap="round"
                             strokeLinejoin="round"
                           />
                           <path
                             d="M14 9C14 10.0609 13.5786 11.0783 12.8284 11.8284C12.0783 12.5786 11.0609 13 10 13C8.93913 13 7.92172 12.5786 7.17157 11.8284C6.42143 11.0783 6 10.0609 6 9"
-                            stroke="currentColor"
+                            stroke="black"
                             strokeWidth="1.5"
                             strokeLinecap="round"
                             strokeLinejoin="round"
