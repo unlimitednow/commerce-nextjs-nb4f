063037948728d2f049bbfb4637a7b76e2f0f816e
Admin <support@unlimitpotential.com>
2023-05-02 20:34:50 -0400

    j

--- site/components/search.tsx
+++ site/components/search.tsx
@@ -432,7 +432,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
       <div className=" 2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 py-9 px-4">
         <div className=" text-center">
           <h2 className=" font-semibold lg:text-4xl text-3xl lg:leading-9 md:leading-7 leading-9 text-gray-800 md:w-full w-9/12 mx-auto">
-            Follow Us on Instagram
+            Follow Us On Instagram
           </h2>
           <p className=" font-normal text-base leading-6 text-gray-600 mt-4 lg:w-5/12 md:w-9/12 mx-auto">
             Follow us on instagram @
