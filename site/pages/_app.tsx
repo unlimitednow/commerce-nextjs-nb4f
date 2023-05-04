ac5cdd55b839f99325913ec9bf00f7e11a5b30d1
Admin <support@unlimitpotential.com>
2023-05-04 06:35:56 -0400

    s

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -14,16 +14,20 @@ import { ManagedUIContext } from '@components/ui/context'
 import { builder, Builder, withChildren } from '@builder.io/react'
 import { useTheme } from 'next-themes'
 import { AuthProvider } from '@descope/react-sdk'
+import { useRouter } from 'next/router'
 
 const projectId = process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID
 
 builder.init('ba26b1f01a7a45cdbbff41a67447be22')
+
+const privatePages = ['/dashboard']
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
   const { theme, setTheme } = useTheme()
+  const router = useRouter()
 
   useEffect(() => {
     document.body.classList?.remove('loading')
@@ -33,16 +37,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       {' '}
       <ClerkProvider {...pageProps}>
-        <AuthProvider projectId={projectId || 'DEFAULT_PROJECT_ID'}>
-          <div>
-            <Head />
-            <ManagedUIContext>
-              <Layout pageProps={pageProps}>
-                <Component {...pageProps} />
-              </Layout>
-            </ManagedUIContext>
-          </div>
-        </AuthProvider>{' '}
+        {privatePages.includes(router.pathname) ? (
+          <Component {...pageProps} />
+        ) : (
+          <>
+            {' '}
+            <AuthProvider projectId={projectId || 'DEFAULT_PROJECT_ID'}>
+              <div>
+                <Head />
+                <ManagedUIContext>
+                  <Layout pageProps={pageProps}>
+                    <Component {...pageProps} />
+                  </Layout>
+                </ManagedUIContext>
+              </div>
+            </AuthProvider>{' '}
+          </>
+        )}
       </ClerkProvider>
     </>
   )
