4a5f9058c4e0ae971a11d74bba085a8b51dcc1c4
Admin <support@unlimitpotential.com>
2023-05-04 15:28:29 -0400

    s

--- site/pages/api/getAllSites/github.ts
+++ site/pages/api/getAllSites/github.ts
@@ -1,20 +1,26 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { requireSession } from '@clerk/nextjs/api'
+import { NextApiResponse, NextApiRequest } from 'next'
+import { ServerGetToken } from '@clerk/types'
 import prisma from '../../../utils/prisma'
+import { requireAuth } from '@clerk/nextjs/api'
 
-export default requireSession(
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    // @ts-ignore
-    const userId = req.session.userId
-
-    const sites = await prisma.orders.findMany({
-      where: {
-        createdBy: userId,
-      },
-    })
-
-    console.log(sites)
-
-    res.json(sites)
+interface ClerkRequest extends NextApiRequest {
+  auth: {
+    userId?: string | null
+    sessionId?: string | null
+    getToken: ServerGetToken
   }
-)
+}
+
+export default requireAuth(async (req: ClerkRequest, res: NextApiResponse) => {
+  const userId = req.session.userId
+
+  const sites = await prisma.orders.findMany({
+    where: {
+      createdBy: userId,
+    },
+  })
+
+  console.log(sites)
+
+  res.json(sites)
+})
