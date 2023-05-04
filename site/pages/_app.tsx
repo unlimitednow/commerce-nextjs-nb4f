352ae1e8d6c751f96ac6413523923a4f4157a550
Admin <support@unlimitpotential.com>
2023-05-04 15:17:05 -0400

    s

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,19 +1,18 @@
-import '@assets/main.css'
-import '@assets/chrome-bug.css'
-import 'keen-slider/keen-slider.min.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { FC, ReactNode, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
-import { builder, Builder, withChildren } from '@builder.io/react'
 import { useTheme } from 'next-themes'
 import { AuthProvider } from '@descope/react-sdk'
-import { useRouter } from 'next/router'
+
+import '@assets/main.css'
+import '@assets/chrome-bug.css'
+import 'keen-slider/keen-slider.min.css'
+
+declare module '*.css'
 
 const projectId = process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID
-
-builder.init('ba26b1f01a7a45cdbbff41a67447be22')
 const clerk_pub_key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
@@ -28,7 +27,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {' '}
       <ClerkProvider
         frontendApi="clerk.nb4fshop.com"
         publishableKey={clerk_pub_key}
