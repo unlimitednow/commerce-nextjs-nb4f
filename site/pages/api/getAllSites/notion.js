9ed8647d7356fdeb83355696016396b791bff821
Admin <support@unlimitpotential.com>
2023-05-04 15:42:27 -0400

    s

--- site/pages/api/getAllSites/notion.js
+++ site/pages/api/getAllSites/notion.js
@@ -1,6 +1,6 @@
-import { withAuth } from '@clerk/nextjs/api'
+import { requireAuth } from '@clerk/nextjs/api'
 
-export default withAuth(async (req, res) => {
+export default requireAuth(async (req, res) => {
   const { userId } = req.auth
   // Load any data your application needs for the API route
   res.status(200).json({ data })
