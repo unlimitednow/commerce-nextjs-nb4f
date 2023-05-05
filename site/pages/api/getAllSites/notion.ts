7b5bde22f64dfa00fd978a9ac412c95c0c7793f1
Admin <support@unlimitpotential.com>
2023-05-04 22:55:56 -0400

    s

--- site/pages/api/getAllSites/notion.ts
+++ site/pages/api/getAllSites/notion.ts
@@ -1,20 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { requireSession } from '@clerk/nextjs/api';
-import prisma from '@/utils/prisma';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { requireSession } from '@clerk/nextjs/api'
+import prisma from '../../../utils/prisma'
 
 export default requireSession(
   async (req: NextApiRequest, res: NextApiResponse) => {
     // @ts-ignore
-    const userId = req.session.userId;
+    const userId = req.session.userId
 
     const sites = await prisma.wishlist.findMany({
       where: {
         createdBy: userId,
       },
-    });
+    })
 
-    console.log(sites);
+    console.log(sites)
 
-    res.json(sites);
+    res.json(sites)
   }
-);
+)
