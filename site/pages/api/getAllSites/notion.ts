3694110d4530fa0bed64a280cf51d6db32f64a9d
Admin <support@unlimitpotential.com>
2023-05-04 23:45:08 -0400

    s

--- site/pages/api/getAllSites/notion.ts
+++ site/pages/api/getAllSites/notion.ts
@@ -2,21 +2,28 @@ import { getAuth } from '@clerk/nextjs/server'
 import prisma from '../../../utils/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Site = {
+  // define the structure of the Site object here
+  // for example:
+  id: number
+  name: string
+  url: string
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { userId } = getAuth(req)
 
-  let sites
+  let sites: Site[] = [] // add a type annotation to the sites variable
+
   if (userId) {
     sites = await prisma.wishlist.findMany({
       where: {
         createdBy: userId,
       },
     })
-  } else {
-    sites = []
   }
 
   console.log(sites)
